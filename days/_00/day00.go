3ae95213c6d5440b59d77274b47a3fce34ebc5df
lloydrichards <lloyd.d.richards@gmail.com>
2024-12-05 15:09:57 +0100

    chore: :memo: att template

---
+++ days/_00/day00.go
@@ -0,0 +1,23 @@
+package day00
+
+import (
+	U "advent-of-code-2024/internal/utils"
+)
+
+func Part1(dir string) (int, error) {
+	input, err := U.LoadInputFile(dir)
+	if err != nil {
+		return -1, err
+	}
+
+	return -1, nil
+}
+
+func Part2(dir string) (int, error) {
+	input, err := U.LoadInputFile(dir)
+	if err != nil {
+		return -1, err
+	}
+
+	return -1, nil
+}
